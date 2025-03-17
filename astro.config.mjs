ebc67c4cd9890b8d6a7b72228a9dfaa16e6edb28
ajaikumarvs <ajaikumarvs@gmail.com>
2025-03-18 03:49:32 +0530

    deployment updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import alpinejs from '@astrojs/alpinejs';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://facein.in', 
   integrations: [
     tailwind(),
     alpinejs()
